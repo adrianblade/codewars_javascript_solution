e3406d8b7643c6720e2b041117e5832c4f652349
Adrian Catalan <adrianblade6@gmail.com>
2019-06-26 21:56:55 +0200

    Add javascript kata monkey count

--- 8kyu/count_the_monkeys/count_the_monkeys.js
+++ 8kyu/count_the_monkeys/count_the_monkeys.js
@@ -10,3 +10,15 @@ exports.addTested = function monkeyCount(n) {
   }
   return arr.reverse();
 }
+
+function monkeyCount2(n) {
+  return Array.from({length:n}, (_,i)=>i+1)
+}
+
+function monkeyCount3(n) {
+  var arr = [];
+  for(var i=1; i<n+1; i++){
+    arr.push(i);
+  }
+  return arr;
+}
