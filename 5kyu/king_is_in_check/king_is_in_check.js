044f359879b0f6a2c32d8b3a93c9cb6a820643a8
Adrian Catalan <adrian.catalan@travel-ims.com>
2020-02-03 22:59:07 +0100

    [5kyu] add chessboard solution

--- 5kyu/king_is_in_check/king_is_in_check.js
+++ 5kyu/king_is_in_check/king_is_in_check.js
@@ -2,19 +2,65 @@ exports.addTested = function kingIsInCheck (chessboard) {
     for(var i = 0; i < chessboard.length; i++) {
       var line = chessboard[i];
       for(var j = 0; j < line.length; j++) {
+        if (chessboard[i][j] === '♟') {
+          return checkPeon(chessboard, i, j);
+        }
+        if (chessboard[i][j] === '♝') {
+          return checkAlfil(chessboard, i, j);
+        }
+        if (chessboard[i][j] === '♜') {
+          return checkTorre(chessboard, i, j);
+        }
+        if (chessboard[i][j] === '♞') {
+          return checkCaballo(chessboard, i, j);
+        }
+        if (chessboard[i][j] === '♛') {
+          return checkQueen(chessboard, i, j);
+        }
           //console.log("[" + i + "][" + j + "] = " + line[j]);
       }
     }
     return false ; // do your magic :)
   }
 
+  function checkCaballo(chessboard, i, j) {
+
+  }
+
+  function checkPeon(chessboard, i, j) {
+    return chessboard[i-1][j+1] === '♔' 
+        || chessboard[i][j+1] === '♔'
+        || chessboard[i+1][j+1] === '♔'
+        || chessboard[i-1][j] === '♔'
+        || chessboard[i][j] === '♔'
+        || chessboard[i+1][j] === '♔'
+        || chessboard[i-1][j-1] === '♔'
+        || chessboard[i][j-1] === '♔'
+        || chessboard[i+1][j-1] === '♔';
+  }
+
+  function checkQueen(chessboard, i, j) {
+
+  }
+
+  function checkTorre(chessboard, i, j) {
+
+  }
+
+  function checkAlfil(chessboard, i, j) {
+    return chessboard[i-1][j-1] === '♔' 
+        || chessboard[i-1][j+1] === '♔'
+        || chessboard[i+1][j-1] === '♔'
+        || chessboard[i+1][j+1] === '♔';
+  }
+
+  // OTHER FUNCTIONS
   exports.printChessboard = function printChessboard(chessboard) {
     appendString='\n';
     for(var i = 0; i < chessboard.length; i++) {
       appendString += "|---|---|---|---|---|---|---|---|\n";
-      var line = chessboard[i];
-      for(var j = 0; j < line.length; j++) {
-        appendString += "| " + line[j] + " ";
+      for(var j = 0; j < chessboard[i].length; j++) {
+        appendString += "| " + chessboard[i][j] + " ";
       }
       appendString += "|\n";
     }
--- README.md
+++ README.md
@@ -6,6 +6,5 @@
 ## My Solutions
 | kyu | Codewars Kate | My Solutions |
 | --- | --- | --- |
-| 5 | [King is in check](https://www.codewars.com/kata/5e28ae347036fa001a504bbe) | [king_is_in_check.js]() |
-
+| 5 | [King is in check](https://www.codewars.com/kata/5e28ae347036fa001a504bbe) | [king_is_in_check.js](https://github.com/adrianblade/codewars_javascript_solution/blob/master/5kyu/king_is_in_check/king_is_in_check.js) |
 | 8 | [Count the monkeys](https://www.codewars.com/kata/56f69d9f9400f508fb000ba7) | [couny_the_monkeys.js](https://github.com/adrianblade/codewars_javascript_solution/8kyu/couny_the_monkeys/couny_the_monkeys.js) |
